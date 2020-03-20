37434dfba1ef8829a1723b80f6201dcce628095b
Michael Mueller <michael.mueller@hpe.com>
2020-03-20 15:52:08 +0100

    Big progress on emergeny and google location + knowledge graph

--- src/pda.ts
+++ src/pda.ts
@@ -28,7 +28,7 @@ const geocoderOptions = {
 
   // Optional depending on the providers
   httpAdapter: 'https', // Default
-  apiKey: 'AIzaSyBQl5zPgrZ4YsXDIYo6CWMRrGNlmmlLy4o', // for Mapquest, OpenCage, Google Premier
+  apiKey: process.env.GOOGLE_KEY, // for Mapquest, OpenCage, Google Premier
   formatter: null, // 'gpx', 'string', ...
 };
 
@@ -63,11 +63,59 @@ telegramBot.on('message', (msg) => {
         // send a message to the user containing city and country
         telegramBot.sendMessage(chatId, `You're located in ${city}, ${country}.`);
 
+        // get emergency numbers
+        let phone = '';
+        http.get(`http://emergencynumberapi.com/api/country/${countryCode}`, (resNumber) => {
+          resNumber.setEncoding('utf8');
+
+          resNumber.on('data', (chunk) => {
+            phone += chunk;
+          });
+
+          resNumber.on('end', () => {
+            const emergency = JSON.parse(phone);
+
+            const phoneArray = [];
+
+            if (emergency.data.member_112 === true) {
+              phoneArray.push('112 (general)');
+            }
+            if (emergency.data.ambulance.all[0] !== '') {
+              phoneArray.push(`${emergency.data.ambulance.all[0]} (ambulance)`);
+            }
+
+            if (emergency.data.police.all[0] !== '') {
+              phoneArray.push(`${emergency.data.police.all[0]} (police)`);
+            }
+
+            if (emergency.data.fire.all[0] !== '') {
+              phoneArray.push(`${emergency.data.fire.all[0]} (fire)`);
+            }
+
+            let emergencyStr = '';
+
+            if (phoneArray.length === 0) {
+              // do nothing
+            } else if (phoneArray.length === 1) {
+              emergencyStr += `In case of emergency, call ${phoneArray[0]}.`;
+            } else {
+              emergencyStr += 'In case of emergency, call ';
+              phoneArray.forEach((element) => {
+                if (element !== phoneArray.slice(-1)[0]) {
+                  emergencyStr += (`${element}, `);
+                } else {
+                  emergencyStr += (`or ${element}.`);
+                }
+              });
+            }
+
+            telegramBot.sendMessage(chatId, emergencyStr);
+          });
+        });
+
+        // use google knowledge base to get short description of location
         googleKnowledgeOptions.path += (`&query=${city}`);
-
         let info = '';
-
-        // use google knowledge base to get short description of city
         https.get(googleKnowledgeOptions, (resInfo) => {
           resInfo.setEncoding('utf8');
           resInfo.on('data', (chunk) => {
@@ -78,20 +126,6 @@ telegramBot.on('message', (msg) => {
             info = JSON.parse(info).itemListElement[0].result.detailedDescription.articleBody;
             // send short description to user
             telegramBot.sendMessage(chatId, info);
-          });
-        });
-
-        let phone = '';
-
-        http.get(`http://emergencynumberapi.com/api/country/${countryCode}`, (resNumber) => {
-          resNumber.setEncoding('utf8');
-
-          resNumber.on('data', (chunk) => {
-            phone += chunk;
-          });
-
-          resNumber.on('end', () => {
-            console.log(typeof phone);
           });
         });
       });
