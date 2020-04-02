33341d192015f39fd6225436e40c55569e922491
Kunze <sonja.kunze@hpe.com>
2020-04-02 13:38:08 +0200

    Setting up Google Calendar Connector

---
+++ src/connectors/googleCalendar/googleCalendarConnector.ts
@@ -0,0 +1,6 @@
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
