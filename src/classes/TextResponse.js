f2bddd915f73731a73b714dcee1437f7c9f76024
Kunze <sonja.kunze@hpe.com>
2020-04-20 13:49:24 +0200

    Removed deprecated babel config

--- src/classes/TextResponse.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var TextResponse = /** @class */ (function () {
-    function TextResponse(text) {
-        this.text = text;
-    }
-    return TextResponse;
-}());
-exports["default"] = TextResponse;
